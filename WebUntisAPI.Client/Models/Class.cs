8815a6638616a2a65f60e93c329c7c7b4168a14e
Marius Kaufman <110390261+Suiram1701@users.noreply.github.com>
2023-06-09 20:50:01 +0200

    Add get class teachers method

--- WebUntisAPI.Client/Models/Class.cs
+++ WebUntisAPI.Client/Models/Class.cs
@@ -3,9 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using WebUntisAPI.Client.Converter;
+using WebUntisAPI.Client.Exceptions;
 
 namespace WebUntisAPI.Client.Models
 {
@@ -41,5 +44,21 @@ namespace WebUntisAPI.Client.Models
         /// An array of all teachers of the class
         /// </summary>
         public int[] Teachers { get; set; }
+
+        /// <summary>
+        /// Get all taechers of this class
+        /// </summary>
+        /// <param name="client">The client for the request (This should be the same client with that you requested the class)</param>
+        /// <param name="id">Identifier for the request</param>
+        /// <param name="ct">Cancellation token</param>
+        /// <returns>The teachers of the class</returns>
+        /// <exception cref="UnauthorizedAccessException">Thrown when the client aren't not logged in</exception>
+        /// <exception cref="HttpRequestException">Thrown when there was an error while the http request</exception>
+        /// <exception cref="WebUntisException">Thrown when the WebUntis API returned an error</exception>
+        public async Task<Teacher[]> GetTeachersAsync(WebUntisClient client, string id = "getClassTeachers", CancellationToken ct = default)
+        {
+            int[] classTeachers = Teachers;
+            return (await client.GetAllTeachersAsync(id, ct)).Where(t => classTeachers.Contains(t.Id)).ToArray();
+        }
     }
 }
