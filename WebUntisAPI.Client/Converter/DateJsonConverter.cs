47b8791fddf65ea80d6b492dfd2ab12567179fb2
Marius Kaufman <110390261+Suiram1701@users.noreply.github.com>
2023-06-15 15:09:18 +0200

    Add json converter for date time

---
+++ WebUntisAPI.Client/Converter/DateJsonConverter.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebUntisAPI.Client.Extensions;
+
+namespace WebUntisAPI.Client.Converter
+{
+    internal class DateJsonConverter : JsonConverter<DateTime>
+    {
+        public override DateTime ReadJson(JsonReader reader, Type objectType, DateTime existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            JToken token = JToken.Load(reader);
+            return new DateTime().FromWebUntisTimeFormat(token.Value<string>(), "000");
+        }
+
+        public override void WriteJson(JsonWriter writer, DateTime value, JsonSerializer serializer)
+        {
+            value.ToWebUntisTimeFormat(out string dateString, out _);
+            writer.WriteValue(dateString);
+        }
+    }
+}
